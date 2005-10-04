b08794532c4f0d9505915a8540606ec036b0e1c7
mrohrmoser <mrohrmoser@9cfae487-02f1-0310-8c2a-d8c834cabcda>
2005-10-04 16:44:48 +0000

    minor version++, demo applications + applets renamed

--- src/jcurl/core/gui/SimpleKeys.java
+++ src/jcurl/core/gui/SimpleKeys.java
@@ -29,7 +29,7 @@ import jcurl.core.TargetDiscrete;
  * A first, simple keyboard input class. Uses a
  * {@link jcurl.core.gui.RealTimePlayer}to play.
  * 
- * @see jcurl.core.gui.JCurlPanelDemo
+ * @see jcurl.core.gui.demo0.ViewerApp
  * @author <a href="mailto:jcurl@gmx.net">M. Rohrmoser </a>
  * @version $Id$
  */
