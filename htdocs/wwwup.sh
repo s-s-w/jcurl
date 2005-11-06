de7559f1f228f01b9baf365f10bdab0e2a035d41
mrohrmoser <mrohrmoser@9cfae487-02f1-0310-8c2a-d8c834cabcda>
2005-11-06 23:26:55 +0000

    ide note

--- htdocs/wwwup.sh
+++ htdocs/wwwup.sh
@@ -4,13 +4,13 @@ dir=/home/groups/jcurl/htdocs
 #dir=/home/m/eclipse/berlios/htdocs
 tmp=$HOME/api.tmp
 
-if [ `svn status --show-updates --quiet $dir | wc --lines` -lt 2 ]
+if [ `svn update $dir | wc --lines` -lt 2 ]
 then
 	echo "Nothing new there!"
     exit 0
 fi    
 
-svn update $dir
+#svn update $dir
 svn status $dir
 mkdir $tmp
 unzip $dir/jar/jcurl-doc-0.2.war -d $tmp > /dev/null
