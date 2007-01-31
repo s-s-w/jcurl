0234e2740a47869af84a741e2854767950f4e83e
mrohrmoser <mrohrmoser@9cfae487-02f1-0310-8c2a-d8c834cabcda>
2007-01-31 00:40:16 +0000

    minor

--- jcurl-demo/src/main/java/org/jcurl/demo/tactics/TacticsNB.java
+++ jcurl-demo/src/main/java/org/jcurl/demo/tactics/TacticsNB.java
@@ -24,24 +24,24 @@ public class TacticsNB extends javax.swing.JFrame {
      */
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
-        org.jcurl.core.swing.PositionDisplay positionDisplay1;
+        org.jcurl.core.swing.PositionDisplay rocksPosDisplay;
 
-        positionDisplay1 = new org.jcurl.core.swing.PositionDisplay();
+        rocksPosDisplay = new org.jcurl.core.swing.PositionDisplay();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        positionDisplay1.setName("The Rocks");
+        rocksPosDisplay.setName("The Rocks");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(positionDisplay1, javax.swing.GroupLayout.PREFERRED_SIZE, 358, Short.MAX_VALUE)
+                .addComponent(rocksPosDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, 358, Short.MAX_VALUE)
                 .addContainerGap(98, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(positionDisplay1, javax.swing.GroupLayout.DEFAULT_SIZE, 165, Short.MAX_VALUE)
+            .addComponent(rocksPosDisplay, javax.swing.GroupLayout.DEFAULT_SIZE, 165, Short.MAX_VALUE)
         );
         pack();
     }// </editor-fold>//GEN-END:initComponents
