beaea6a82c8308e9beae1c5fd7d9f3f1ecffa35a
mrohrmoser <mrohrmoser@9cfae487-02f1-0310-8c2a-d8c834cabcda>
2007-02-28 23:36:36 +0000

    AntLR Demo added

---
+++ jcurl-demo/src/test/java/org/jcurl/ExprParserTest.java
@@ -0,0 +1,16 @@
+package org.jcurl;
+
+import java.io.StringReader;
+
+import junit.framework.TestCase;
+import antlr.RecognitionException;
+import antlr.TokenStreamException;
+
+public class ExprParserTest extends TestCase {
+
+	public void test010() throws RecognitionException, TokenStreamException {
+		final String s = "1+2";
+		final ExprParser p = new ExprParser(new ExprLexer(new StringReader(s)));
+		assertEquals(3, p.expr());
+	}
+}
