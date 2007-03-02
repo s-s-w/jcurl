6a12f04a6544a5c67591069a8dbebaec9f5e3bd4
mrohrmoser <mrohrmoser@9cfae487-02f1-0310-8c2a-d8c834cabcda>
2007-03-02 01:12:34 +0000

    added link to homepage

--- src/main/java/org/jcurl/math/CurveShape.java
+++ src/main/java/org/jcurl/math/CurveShape.java
@@ -1,5 +1,5 @@
 /*
- * jcurl curling simulation framework Copyright (C) 2005 M. Rohrmoser
+ * jcurl curling simulation framework http://www.jcurl.orgCopyright (C) 2005 M. Rohrmoser
  * 
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
