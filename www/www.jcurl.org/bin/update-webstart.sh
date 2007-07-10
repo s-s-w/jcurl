91e31a370a21b015ad75d39f110bfa58c4297b4d
mrohrmoser <mrohrmoser@9cfae487-02f1-0310-8c2a-d8c834cabcda>
2007-07-10 22:46:46 +0000

    Add a cronnable webstart updater

---
+++ www/www.jcurl.org/bin/update-webstart.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copy given webstart apps from the maven repo to the webspace.
+
+groupId=org.jcurl.demo
+version=0.7.3
+modules="viewer tactics"
+
+srczipbase=$HOME/.m2/repository
+srczipbase=$HOME/org.jcurl.www/m2/repo
+dstwebbase=$HOME/jnlp
+
+for m in $modules ; do
+	src=$srczipbase/${groupId//.//}/$m/$version/$m-$version.zip
+	dst=$dstwebbase/${groupId//.//}/$m/$version
+#	ls -Al $src $dst
+	mkdir -p $dst
+	rm -rf $dst
+	mkdir -p $dst
+	unzip $src -d $dst
+done
