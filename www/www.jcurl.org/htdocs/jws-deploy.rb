67068298248143dac6d613ee21f62eb1c2369fe7
mrohrmoser <mrohrmoser@9cfae487-02f1-0310-8c2a-d8c834cabcda>
2007-08-19 14:54:50 +0000

    pull in snapshots.

--- www/www.jcurl.org/htdocs/jws-deploy.rb
+++ www/www.jcurl.org/htdocs/jws-deploy.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/ruby
 
 class DeployWebstart
-	def initialize repobase="#{ENV['HOME']}/org.jcurl.www/m2/repo", dstbase="#{ENV['HOME']}/org.jcurl.www/jws"
+	def initialize repobase="#{ENV['HOME']}/org.jcurl.www/m2/repo", snapbase="#{ENV['HOME']}/org.jcurl.www/m2/snap", dstbase="#{ENV['HOME']}/org.jcurl.www/jws"
 		@repobase = repobase
+		@snapbase = snapbase.nil? ? @repobase : snapbase
 		@dstbase = dstbase
 	end
 
@@ -17,8 +18,8 @@ class DeployWebstart
 	end
 
 	def deploy groupId, artifactId, version, packaging="zip"
-		srcbase = "#{@repobase}/#{groupId.tr '.', '/'}/#{artifactId}/#{version}"
 		ver = /^(.+?)(-SNAPSHOT)?$/.match version
+		srcbase = "#{ver[2] ? @snapbase : @repobase}/#{groupId.tr '.', '/'}/#{artifactId}/#{version}"
 		if ver[2]
 			# find the most recent entry
 			src = Dir["#{srcbase}/#{artifactId}-#{ver[1]}-*-*.#{packaging}"].sort().reverse()[0]
@@ -33,7 +34,7 @@ class DeployWebstart
     end
 end
 
-o = DeployWebstart.new "/home/m/.m2/repository", "/home/m/tmp"
+o = DeployWebstart.new "/home/m/.m2/repository", nil, "/home/m/tmp"
 o = DeployWebstart.new
 
 o.deploy "org.jcurl.demo", "tactics", "0.7-SNAPSHOT"
