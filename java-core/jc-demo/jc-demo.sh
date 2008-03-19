0556b9e9e83a3c339c8bcae8ac47a7a8523e8bb2
mrohrmoser <mrohrmoser@9cfae487-02f1-0310-8c2a-d8c834cabcda>
2008-03-19 12:38:51 +0000

    project renamed

--- java-core/jc-demo/jc-demo.sh
+++ java-core/jc-demo/jc-demo.sh
@@ -4,7 +4,7 @@ cp=$cp:$HOME/.m2/repository/commons-logging/commons-logging/1.0.3/commons-loggin
 cp=$cp:$HOME/.m2/repository/edu/umd/cs/hcil/piccolo/1.2/piccolo-1.2.jar
 cp=$cp:$HOME/.m2/repository/org/jcurl/core/jc-core/0.7-SNAPSHOT/jc-core-0.7-SNAPSHOT.jar
 cp=$cp:$HOME/.m2/repository/org/jcurl/core/jc-xsio/0.7-SNAPSHOT/jc-xsio-0.7-SNAPSHOT.jar
-cp=$cp:$HOME/.m2/repository/org/jcurl/core/jc-zui/0.7-SNAPSHOT/jc-zui-0.7-SNAPSHOT.jar
+cp=$cp:$HOME/.m2/repository/org/jcurl/core/jc-piccolo/0.7-SNAPSHOT/jc-piccolo-0.7-SNAPSHOT.jar
 cp=$cp:$HOME/.m2/repository/org/jcurl/core/jc-demo/0.7-SNAPSHOT/jc-demo-0.7-SNAPSHOT.jar
 cp=$cp:$HOME/.m2/repository/xpp3/xpp3_min/1.1.3.4.O/xpp3_min-1.1.3.4.O.jar
 #java -Xprof -cp $cp org.jcurl.demo.zui.BroomPromptDemo
