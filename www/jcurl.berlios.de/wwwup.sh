16445834cd02867b641e45b91deed6dfa7d7f6ca
mro <mro@9cfae487-02f1-0310-8c2a-d8c834cabcda>
2008-07-03 03:54:12 +0000

    do the svn repo backup using git.

--- www/jcurl.berlios.de/wwwup.sh
+++ www/jcurl.berlios.de/wwwup.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 # $Id: wwwup.sh 231 2006-02-19 13:12:31Z mrohrmoser $
 
-# Push the svn repo dump to jcurl.org as a sort of backup
-repo=jcurl-repos.gz
-echo "backup http://svn.berlios.de/svndumps/$repo to ftp://jcurl.org"
-wget --no-verbose -O $repo http://svn.berlios.de/svndumps/$repo
-echo -e "cd public_html\nput $repo" | ftp -i -V jcurl.org
-rm $repo
-
-echo "done"
-echo "==============="
+# the following is done at my private workstation using git:
+#
+## Push the svn repo dump to jcurl.org as a sort of backup
+#repo=jcurl-repos.gz
+#echo "backup http://svn.berlios.de/svndumps/$repo to ftp://jcurl.org"
+#wget --no-verbose -O $repo http://svn.berlios.de/svndumps/$repo
+#echo -e "cd public_html\nput $repo" | ftp -i -V jcurl.org
+#rm $repo
+#
+#echo "done"
+#echo "==============="
 
 echo "update the local working copy (the http://jcurl.berlios.de/ content)"
 dir=$WWWHOME
