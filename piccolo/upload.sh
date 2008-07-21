89247948315097507e4176a85495481c1613e5c1
mro <mro@9cfae487-02f1-0310-8c2a-d8c834cabcda>
2008-07-21 10:56:12 +0000

    pom info update

--- piccolo/upload.sh
+++ piccolo/upload.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# kopiert Piccolo Jars in das jcurl Maven2 Repository
+# upload piccolo jars to the jcurl maven2 repository
 
-mvnRepo=http://www.jcurl.org/m2/repo
-mvnScp=scp://jcurl.org/kunden/homepages/6/d143911421/htdocs/org.jcurl.www/m2/repo
+mvnScp=scp://shell.berlios.de/home/groups/jcurl/htdocs/m2/repo
+piccoloBase=$HOME/work/piccolo2d/piccolo-1.2.1
 packaging=jar
 
 function mvnup {
@@ -11,25 +11,25 @@ function mvnup {
         -Dversion=$version \
         -Dpackaging=$packaging \
         -Dfile=$srcFile \
-        -DrepositoryId=jcurl-repository \
+        -DrepositoryId=ssh-jcurl-repo \
         -Durl=$mvnScp
 }
 
 function mvndeploy {
 	mvn deploy:deploy-file -DpomFile=$pomFile \
         -Dfile=$srcFile \
-        -DrepositoryId=jcurl-repository \
+        -DrepositoryId=ssh-jcurl-repo \
         -Durl=$mvnScp
 }
 
-srcFile=/home/m/tmp/piccolo-1.2/build/piccolo.jar
+srcFile=$piccoloBase/build/piccolo.jar
 pomFile=piccolo.pom.xml
 mvndeploy
 
-srcFile=/home/m/tmp/piccolo-1.2/build/piccolox.jar
+srcFile=$piccoloBase/build/piccolox.jar
 pomFile=piccolox.pom.xml
 mvndeploy
 
-srcFile=/home/m/tmp/piccolo-1.2/build/examples.jar
+srcFile=$piccoloBase/build/examples.jar
 pomFile=examples.pom.xml
 mvndeploy
